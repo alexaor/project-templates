cff029895c1c53401688d044ff9fb7b2bfb51823
Alexander Orvik <alexao1111@gmail.com>
2022-06-29 20:31:07 +0200

    :tada: mantine react project

---
+++ react-vite-mantine/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1>BAREBONES REACT TYPESCRIPT VITE PROJECT</h1>
+    </div>
+  )
+}
+
+export default App
